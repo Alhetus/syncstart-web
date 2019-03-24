ad81f18685456841ec7d71f59e9f1787657dca98
Mika Viinamäki <mika.viinamaki@fraktio.fi>
2019-03-24 17:31:35 +0200

    Yay it works maybe

---
+++ backend/index.js
@@ -0,0 +1,171 @@
+const SYNCSTART_UDP_PORT = 53000;
+const WEBSOCKET_PORT = 8080;
+const MAX_POSSIBLE_DANCE_POINTS_DIFFERENCE = 100;
+
+const _ = require("lodash");
+const WebSocket = require("ws");
+const dgram = require("dgram");
+
+const udpServer = dgram.createSocket({ type: "udp4", reuseAddr: true });
+
+const wsServer = new WebSocket.Server({
+  port: WEBSOCKET_PORT
+});
+
+let serverState = null;
+
+const parseMessage = msg => {
+  const [
+    // "misc" information
+    song,
+    playerNumber,
+    playerName,
+    actualDancePoints,
+    currentPossibleDancePoints,
+    possibleDancePoints,
+    formattedScore,
+    life,
+    isFailed,
+
+    // tap note scores
+    tapNoteNone,
+    tapNoteHitMine,
+    tapNoteAvoidMine,
+    tapNoteCheckpointMiss,
+    tapNoteMiss,
+    tapNoteW5,
+    tapNoteW4,
+    tapNoteW3,
+    tapNoteW2,
+    tapNoteW1,
+    tapNoteCheckpointHit,
+
+    // hold note scores
+    holdNoteNone,
+    holdNoteLetGo,
+    holdNoteHeld,
+    holdNoteMissed
+  ] = msg.split("|");
+
+  return {
+    song,
+    playerNumber: parseInt(playerNumber, 10),
+    playerName,
+    actualDancePoints: parseInt(actualDancePoints, 10),
+    currentPossibleDancePoints: parseInt(currentPossibleDancePoints, 10),
+    possibleDancePoints: parseInt(possibleDancePoints, 10),
+    formattedScore,
+    life: parseFloat(life),
+    isFailed: isFailed === "1" ? true : false,
+
+    tapNote: {
+      none: parseInt(tapNoteNone, 10),
+      hitMine: parseInt(tapNoteHitMine, 10),
+      avoidMine: parseInt(tapNoteAvoidMine, 10),
+      checkpointMiss: parseInt(tapNoteCheckpointMiss, 10),
+      miss: parseInt(tapNoteMiss, 10),
+      W5: parseInt(tapNoteW5, 10),
+      W4: parseInt(tapNoteW4, 10),
+      W3: parseInt(tapNoteW3, 10),
+      W2: parseInt(tapNoteW2, 10),
+      W1: parseInt(tapNoteW1, 10),
+      checkpointHit: parseInt(tapNoteCheckpointHit, 10)
+    },
+
+    holdNote: {
+      none: parseInt(holdNoteNone, 10),
+      letGo: parseInt(holdNoteLetGo, 10),
+      held: parseInt(holdNoteHeld, 10),
+      missed: parseInt(holdNoteMissed, 10)
+    }
+  };
+};
+
+const clampPercentage = val => _.clamp(val, 0, 1);
+
+const sortScores = (score1, score2) => {
+  // if other is one is failed and other not, that's all that matters
+  if (score1.isFailed !== score2.isFailed) {
+    return (score1.isFailed ? 1 : 0) - (score2.isFailed ? 1 : 0);
+  }
+
+  const overPossibleDancePointDifference =
+    Math.abs(
+      score1.currentPossibleDancePoints - score2.currentPossibleDancePoints
+    ) > MAX_POSSIBLE_DANCE_POINTS_DIFFERENCE;
+
+  let firstPercentage = 0;
+  let secondPercentage = 0;
+
+  if (overPossibleDancePointDifference) {
+    firstPercentage = score1.actualDancePoints / score1.possibleDancePoints;
+    secondPercentage = score2.actualDancePoints / score2.possibleDancePoints;
+  } else {
+    firstPercentage =
+      score1.actualDancePoints / score1.currentPossibleDancePoints;
+    secondPercentage =
+      score2.actualDancePoints / score2.currentPossibleDancePoints;
+  }
+
+  return clampPercentage(secondPercentage) - clampPercentage(firstPercentage);
+};
+
+const processMessage = (address, msg) => {
+  const parsedMessage = parseMessage(msg);
+  const scoreKey = `${address} ${parsedMessage.playerNumber}`;
+  const scoreData = Object.assign({}, parsedMessage, { id: scoreKey });
+
+  if (serverState === null || serverState.currentSong !== parsedMessage.song) {
+    // song changed, reset server state
+    serverState = {
+      currentSong: parsedMessage.song,
+      scores: {
+        [scoreKey]: scoreData
+      },
+      sortedScores: [scoreData]
+    };
+  } else {
+    // otherwise just update new scores
+    serverState.scores[scoreKey] = scoreData;
+    serverState.sortedScores = Object.values(serverState.scores);
+    serverState.sortedScores.sort(sortScores);
+  }
+};
+
+const getClientMessage = () =>
+  JSON.stringify({
+    song: serverState.song,
+    scores: serverState.sortedScores
+  });
+
+udpServer.on("message", (buffer, rinfo) => {
+  // we are interested only in score messages
+  if (buffer[0] !== 0x02) {
+    return;
+  }
+
+  const scoreMessage = buffer.slice(1).toString("utf-8");
+
+  try {
+    processMessage(rinfo.address, scoreMessage);
+  } catch (e) {
+    console.error(`ERROR: couldn't process message '${scoreMessage}'`, e);
+  }
+
+  const scoreStateForClients = getClientMessage();
+
+  wsServer.clients.forEach(client => {
+    client.send(scoreStateForClients);
+  });
+});
+
+wsServer.on("connection", wsClient => {
+  if (serverState) {
+    wsClient.send(getClientMessage());
+  }
+});
+
+console.log("Starting server!");
+console.log("SYNCSTART_UDP_PORT:", SYNCSTART_UDP_PORT);
+console.log("WEBSOCKET_PORT:", WEBSOCKET_PORT);
+udpServer.bind(SYNCSTART_UDP_PORT);
---
+++ frontend/src/App.js
@@ -0,0 +1,162 @@
+import React from "react";
+import Websocket from "react-websocket";
+import styled, { css, createGlobalStyle } from "styled-components";
+import chroma from "chroma-js";
+import { useSpring, animated, config } from "react-spring";
+import "reset-css";
+
+const websocketUrl =
+  process.env.REACT_APP_WEBSOCKET_URL || "ws://localhost:8080/";
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Exo:300,400,500,600,700,800');
+
+  body {
+    font-family: "Exo";
+    background-color: black;
+  }
+`;
+
+const Bars = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: stretch;
+  align-items: flex-start;
+  height: 100%;
+  width: 100%;
+  max-width: 400px;
+`;
+
+const BarContainer = styled.div`
+  width: 100%; /* will get overriden */
+  font-weight: 500;
+  color: white;
+  display: flex;
+  align-items: center;
+  position: relative;
+  padding: 8px;
+  transition: opacity 100ms;
+  font-size: 25px;
+  text-shadow: rgba(0, 0, 0, 0.5) 1px 1px 2px;
+  will-change: opacity;
+
+  &:not(:last-child) {
+    margin-bottom: 2px;
+  }
+
+  ${props =>
+    props.isFailed &&
+    css`
+      opacity: 0.5;
+      color: rgb(255, 50, 50) !important;
+    `}
+`;
+
+const Lifebar = styled(animated.div)`
+  z-index: -1;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  background-color: black;
+`;
+
+const lifebarColor = chroma.scale(["#FF0000", "#0E1323"]).padding([0, -0.5]);
+
+const Bar = React.memo(
+  ({ playerName, life, formattedScore, tapNote, holdNote, isFailed }) => {
+    const props = useSpring({
+      width: life * 100 + "%",
+      backgroundColor: lifebarColor(life).toString(),
+      config: config.stiff
+    });
+
+    return (
+      <BarContainer isFailed={isFailed}>
+        <span>{playerName}</span>
+        <RenderedScore
+          formattedScore={formattedScore}
+          tapNote={tapNote}
+          holdNote={holdNote}
+        />
+        <Lifebar style={props} />
+      </BarContainer>
+    );
+  }
+);
+
+const JudgementContainer = styled.span`
+  font-weight: 400;
+  font-size: 18px;
+`;
+
+const JudgementScore = React.memo(({ color, label, value }) =>
+  value > 0 ? (
+    <JudgementContainer>
+      {value}
+      <span style={{ color: color }}>{label}</span>
+    </JudgementContainer>
+  ) : null
+);
+
+const ScoreContainer = styled.div`
+  margin-left: auto;
+  font-weight: 800;
+  white-space: nowrap;
+  flex-shrink: 0;
+`;
+
+const Score = styled.span``;
+
+const RenderedScore = React.memo(({ formattedScore, tapNote, holdNote }) => {
+  const misses =
+    tapNote.miss + tapNote.hitMine + tapNote.checkpointMiss + holdNote.missed;
+
+  const showExtraScore = [
+    tapNote.W2,
+    tapNote.W3,
+    tapNote.W4,
+    tapNote.W5,
+    misses
+  ].every(val => val < 10);
+
+  return (
+    <ScoreContainer>
+      {showExtraScore && (
+        <>
+          <JudgementScore color="#e29c18" label="e" value={tapNote.W2} />{" "}
+          <JudgementScore color="#66c955" label="g" value={tapNote.W3} />{" "}
+          <JudgementScore color="#5b2b8e" label="d" value={tapNote.W4} />{" "}
+          <JudgementScore color="#c9855e" label="w" value={tapNote.W5} />{" "}
+          <JudgementScore color="#ff0000" label="m" value={misses} />{" "}
+        </>
+      )}
+      <Score>{formattedScore}</Score>
+    </ScoreContainer>
+  );
+});
+
+const App = () => {
+  const [scoreState, setScoreState] = React.useState(null);
+
+  const handleMessage = React.useCallback(
+    msg => {
+      setScoreState(JSON.parse(msg));
+    },
+    [setScoreState]
+  );
+
+  return (
+    <>
+      <GlobalStyles />
+      <Websocket url={websocketUrl} onMessage={handleMessage} />
+      <Bars>
+        {scoreState &&
+          scoreState.scores.map(score => <Bar key={score.id} {...score} />)}
+      </Bars>
+    </>
+  );
+};
+
+export default App;
